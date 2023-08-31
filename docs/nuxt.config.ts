d98725b56c174e49d6fcd7fe04c7db93affc3ff8
Daniel Roe <daniel@roe.dev>
2023-08-31 16:24:11 +0100

    perf: only add prose components as globals

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -34,19 +34,4 @@ export default defineNuxtConfig({
       routes: ['/api/search.json']
     }
   },
-  hooks: {
-    // Related to https://github.com/nuxt/nuxt/pull/22558
-    // Adding all global components to the main entry
-    // To avoid lagging during page navigation on client-side
-    // Downside: bigger JS bundle
-    // With sync: 465KB, gzip: 204KB
-    // Without: 418KB, gzip: 184KB
-    'components:extend' (components) {
-      for (const comp of components) {
-        if (comp.global) {
-          comp.global = 'sync'
-        }
-      }
-    }
-  }
 })
